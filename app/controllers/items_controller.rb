2f4773f4d765b57d1859054c7f083b9916902755
Harrison <harrisonjameskunkel@gmail.com>
2021-03-12 21:06:30 -0500

    run generators and make routes for items

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,22 @@
+class ItemsController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'items/create'
+  get 'items/update'
+  get 'items/show'
+  get 'items/index'
+  get 'items/destroy'
+  get 'items/edit'
+  get 'items/new'
+resources :items
+
 end
