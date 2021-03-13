2f45e79cd0e6a359c51e0d65674ef806ae63a74a
Harrison <harrisonjameskunkel@gmail.com>
2021-03-13 02:36:29 -0500

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'items/create'
-  get 'items/update'
-  get 'items/show'
-  get 'items/index'
-  get 'items/destroy'
-  get 'items/edit'
-  get 'items/new'
+
   
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
